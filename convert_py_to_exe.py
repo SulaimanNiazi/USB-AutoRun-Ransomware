45d328599b7bdf1cae0a62abb9d12a30e282dffc
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2025-04-23 11:46:37 +0500

    Add function for finding .py file

--- convert_py_to_exe.py
+++ convert_py_to_exe.py
@@ -20,6 +20,32 @@ def convert_to_exe(py_file):
     except subprocess.CalledProcessError as e:
         print("Error during conversion:", e)
 
+def find_py_file():
+    # Enter path automatically
+    py_file = "ransomware.py"
+    
+    current_path = os.path.abspath(".")
+    py_file = os.path.join(current_path, py_file)
+
+    if(os.path.isfile(py_file)):
+        return py_file
+    else:
+        # If path is not set in this .py file
+        for file in os.listdir(current_path):
+            if file.endswith(".py") and file != "convert_py_to_exe.py":
+                py_file = os.path.join(current_path, file)
+    
+    if(os.path.isfile(py_file)):
+        return py_file
+    else:
+        # Enter Manually if no .py file is found
+        while True:    
+            py_file = input("Enter the path to your Python (.py) file: ").strip()
+            if os.path.isfile(py_file):
+                return py_file
+            else:
+                print("Error: The specified Python file does not exist.")
+
 if __name__ == "__main__":
-    py_file = input("Enter the path to your Python (.py) file: ").strip()
+    py_file = find_py_file()
     convert_to_exe(py_file)
